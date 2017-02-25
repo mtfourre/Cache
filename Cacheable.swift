171a80257b56347c2c4109b04fab8ba0fc6002e6
Michael Fourre <mtfourre@gmail.com>
2017-02-24 23:05:47 -0800

    Public visibility modifiers for Cacheable protocol

--- Cacheable.swift
+++ Cacheable.swift
@@ -7,59 +7,59 @@
 
 import Foundation
 
-protocol Cacheable
+public protocol Cacheable
 {
     associatedtype K: AnyObject
     associatedtype V: AnyObject
     static var shared: NSCache<K, V> { get }
 }
 
-extension Cacheable
+public extension Cacheable
 {
-    static var cache: NSCache<K, V> {
+    public static var cache: NSCache<K, V> {
         let nsc: NSCache<K, V> = NSCache<K, V>()
         nsc.name = self.name
         return nsc
     }
     
-    static var name: String {
+    public static var name: String {
         return String(describing: self)
     }
     
-    static func get(_ key: K?) -> V?
+    public static func get(_ key: K?) -> V?
     {
         guard let key = key else { return nil }
         return self.shared.object(forKey: key)
     }
     
-    static func put(_ object: V?, at key: K?)
+    public static func put(_ object: V?, at key: K?)
     {
         guard let key = key, let obj = object else { return }
         self.shared.setObject(obj, forKey: key as K)
     }
 }
 
-extension Cacheable where K: NSNumber
+public extension Cacheable where K: NSNumber
 {
-    static func get(_ key: Int?) -> V?
+    public static func get(_ key: Int?) -> V?
     {
         return self.get(K(value: key ?? -1))
     }
     
-    static func put(_ object: V?, at key: Int?)
+    public static func put(_ object: V?, at key: Int?)
     {
         self.put(object, at: K(value: key ?? -1))
     }
 }
 
-extension Cacheable where K: NSString
+public extension Cacheable where K: NSString
 {
-    static func get(_ key: String?) -> V?
+    public static func get(_ key: String?) -> V?
     {
         return self.get(K(string: key ?? ""))
     }
     
-    static func put(_ object: V?, at key: String?)
+    public static func put(_ object: V?, at key: String?)
     {
         self.put(object, at: K(string: key ?? ""))
     }
